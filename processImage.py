323d9eb6338d358808db1e8d1f1978be3b12afad
wnoadiarwb <ottombenson@gmail.com>
2020-07-26 01:47:49 -0400

    Update processImage.py

--- processImage.py
+++ processImage.py
@@ -291,9 +291,9 @@ for y in range(random.randint(1, 4)):
 img = img.convert(mode = "RGB", matrix = None, dither = Image.FLOYDSTEINBERG,
 		palette = Image.WEB, colors = noColors)
 
-# palimage = Image.new('P', (16, 16))
-# palimage.putpalette(paletteRGBCMYK * 32)
-# img = quantizetopalette(img, palimage, dither=Image.FLOYDSTEINBERG)
+palimage = Image.new('P', (16, 16))
+palimage.putpalette(paletteRGBCMYK * random.randint(6, 32))
+img = quantizetopalette(img, palimage, dither=Image.FLOYDSTEINBERG)
 
 # img = img.quantize(noColors, Image.MEDIANCUT, random.randint(0,6), paletteRGBCMYK, dither = Image.FLOYDSTEINBERG)
 
